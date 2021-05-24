5a458d24dd519d231236c0eb37699462164de7fe
hedall89 <henrikvadmanddall@gmail.com>
2021-05-24 15:54:22 +0200

    Creation of packages. Creation of DatabaseConnector. Creation of DBWrapper

---
+++ src/Application/DatabaseConnector.java
@@ -0,0 +1,36 @@
+package Application;
+
+import Domain.Main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseConnector {
+
+    static Connection con;
+
+    public static java.sql.Connection createConnection() {
+        try {
+            con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=XXXXXX", "sa", "123456");
+            System.out.println("Connected");
+        } catch (SQLException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, (String) null, e);
+        }
+        return con;
+    }
+
+    public static Connection getConnection() {
+        return con;
+    }
+
+    public static void manualDisconnect() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Domain/Main.java
@@ -0,0 +1,36 @@
+package Domain;
+
+import Application.DatabaseConnector;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        preloader();
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/TimeToolUI.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void preloader() {
+        DatabaseConnector.createConnection();
+
+
+    }
+
+}
