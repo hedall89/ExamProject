73a1efb34dc8a632aeee7a3fa1d6a7542b3ce7d0
bolonase <bollonase@hotmail.com>
2021-06-03 01:54:58 +0200

    Main version 2

--- src/Domain/Main.java
+++ src/Domain/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+    //asdas
 
     public void preloader() {
         DatabaseConnector.createConnection();
